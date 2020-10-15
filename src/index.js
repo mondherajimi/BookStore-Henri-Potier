79891b568c007d5c0bb5b3a5fb50af0130bbc241
ajimi mondher <ajimimondher@hotmail.com>
2020-10-15 16:16:49 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './maincss.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './main/App'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'))
